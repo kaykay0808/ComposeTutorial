649d81b28285509c4dabc8d8680503571adbfea9
kay <kayshiro@live.no>
2022-05-20 14:27:04 +0200

    hello

--- app/src/main/java/com/kay/composetutorial/MainActivity.kt
+++ app/src/main/java/com/kay/composetutorial/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.kay.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kay.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            ComposeTutorialTheme {
+                MessageCard(Message("Android", "Jetpack Compose"))
+            }
         }
     }
 }
@@ -25,26 +41,57 @@ data class Message(val author: String, val body: String)
 @Composable
 fun MessageCard(msg: Message) {
 
-    Row {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            contentDescription = null,
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                // outline border stroke around the circle
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
         // arrange the text in column
         Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            // Add a vertical space between the author and message texts (space between top text and lower text)
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium/*Corner value*/, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all= 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
-
 }
 
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    name = "Dark Mode",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "hey, take a look at Jetpack compose")
-    )
+    ComposeTutorialTheme {
+        MessageCard(
+            msg = Message("Colleague", "hey, take a look at Jetpack compose")
+        )
+    }
 }
 
 
